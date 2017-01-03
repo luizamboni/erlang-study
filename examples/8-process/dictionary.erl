1c9e5d0ac7f18a1bab5451910dd90e2b3a8f4a73
zamboni <luizamboni2002@hotmail.com>
2017-01-02 22:30:41 -0200

    ADD process dictionary example

--- examples/8-process/dictionary.erl
+++ examples/8-process/dictionary.erl
@@ -1,20 +1,39 @@
--module(dictyonary).
+#!/usr/bin/escript
+
+-module(dictionary).
 
 -export([main/1]).
 
 main(_)->
-  %Associates the Value with the Key, returning the previous value associated with the Key, or the atom undefined.
-  put(Key, Value) -> OldValue
-  %Returns the Value currently associated with the Key, or the atom undefined.
+  
+    io:fwrite("~w~n",[get()])
 
-  get(Key) -> Value
-  %Returns a list of all Key/Value tuples.
-  get() -> [{Key, Value}, ..., {Key, Value}]
-  %Returns a list of Keys having the given Value.
-  get_keys(Value) -> [Key, ..., Key]
-  %Returns the Value currently associated with the Key, or the atom undefined, and removes the Key/Value pair from the process dictionary.
+  % Associates the Value with the Key, 
+  % returning the previous value associated with the Key, 
+  % or the atom undefined.
+  , OldValue = put(test, "test value")
+  % Returns the Value currently 
+  % associated with the Key, or the atom undefined.
 
-  erase(Key) -> Value
-  %Returns a list of all Key/Value tuples, and erases the contents of the process dictionary.
-  erase() -> [{Key, Value}, ..., {Key, Value}]
+  , NewValue = get(test)
+
+  % Returns a list of all Key/Value tuples.
+  , io:fwrite("~w~n",[get()])
+  % Returns a list of Keys having the given Value.
+  , io:fwrite("~w~n",[get_keys(NewValue)])
+  % Returns the Value currently associated with the Key, or the atom undefined, 
+  % and removes the Key/Value pair from the process dictionary.
+
+  , io:fwrite("erasing ~w~n",[erase(test)]) 
+  % Returns a list of all Key/Value tuples, and erases the contents of the process dictionary.
+  , put(test, "test 1")
+  , put(test2, "test 2")
+  , put(test3, "test 3")
+
+  , io:fwrite("~w~n",[get()])
+  
+  , io:fwrite("erase all ~w~n",[erase()])
+
+  , io:fwrite("~w~n",[get()])
+
 .

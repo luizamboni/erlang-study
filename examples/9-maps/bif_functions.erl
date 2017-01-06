8531b94bed506d1f74e67ed89a828daec27ed114
luiz vasconcellos <luizamboni2002@hotmail.com>
2017-01-06 18:53:20 -0200

    bif

--- examples/9-maps/bif_functions.erl
+++ examples/9-maps/bif_functions.erl
@@ -14,4 +14,12 @@ main(_) ->
   , X = maps:find(fun(K,V) -> V =:= K+1 end, M0)
   , io:fwrite("first Y = X + 1 : ~w~n",[X])
 
+  , Acc = maps:fold(fun(K,V,AccIn) -> AccIn + V end,0, M0)
+  , io:fwrite("sum of all Values is: ~w~n",[Acc])
+
+  , L0 = [ {"a",ignored}, {1337,"value two"}, {42,value_three}, {"a",1} ]
+
+  , io:fwrite("list to map : ~w~n",[maps:from_list(L0)])
+  , io:fwrite("get value of key 4 => ~w~n",[maps:get(4,M0)])
+
 .

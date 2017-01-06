0001e6cde50ad6890d18bec8e0b7ecf02e462170
zamboni <lzamboni@admatic.com.br>
2017-01-06 11:18:36 -0200

    ADD bif_functions

--- examples/9-maps/bif_functions.erl
+++ examples/9-maps/bif_functions.erl
@@ -6,5 +6,12 @@
 
 
 main(_) -> 
+    M0 = #{ 1 => 2 , 2 => 3 , 3 => 4, 4 => 6 }
+
+  , M1 = maps:filter(fun(K,V) -> V =:= K+1 end, M0)
+  , io:fwrite("filter Y = X + 1 : ~w~n",[M1])
+
+  , X = maps:find(fun(K,V) -> V =:= K+1 end, M0)
+  , io:fwrite("first Y = X + 1 : ~w~n",[X])
 
 .

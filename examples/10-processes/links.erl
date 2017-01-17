7325a333b8d49a5d1054f3a848da94ba9db85444
luiz vasconcellos <luizamboni2002@hotmail.com>
2017-01-17 11:27:02 -0200

    ADD spawn example

---
+++ examples/10-processes/links.erl
@@ -0,0 +1,32 @@
+-module(links).
+
+-export([main/1]).
+
+
+main(_) ->
+	link(spawn(fun() -> start_process(0) end)),
+	spawn_link(fun() -> start_process(1) end),
+	% if not catched, the error will throw to father proccess 
+	% and it will stop all linked subprocesses
+	% link(spawn(fun() -> catch(start_process(other)) end)),
+  link(spawn(fun() -> start_process(other) end)),
+  io:fwrite("end program~n")
+.
+
+
+start_process(0) -> 
+  io:fwrite("~p~n",[ok]),
+  timer:sleep(1000),
+  start_process(0);
+
+start_process(1) ->   
+  io:fwrite("~p~n",[not_ok]),
+  timer:sleep(1000),
+  start_process(1);
+
+start_process(_) -> 
+  io:fwrite("~p~n",[error]),
+  timer:sleep(500),
+  exit("becouse i want")
+.
+

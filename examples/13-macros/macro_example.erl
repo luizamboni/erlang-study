3202800ab3e8f0bce8cc39ce07a95e68e2292e40
luiz vasconcellos <luizamboni2002@hotmail.com>
2017-02-13 18:26:58 -0200

    ADD macro_example

--- examples/13-macros/macro_example.erl
+++ examples/13-macros/macro_example.erl
@@ -3,9 +3,17 @@
 -module(macro_example).
 
 -export([main/1]).
+
+-ifndef(sum).
 -define(sum(X,Y),{X+Y}). 
+-endif.
+
+-ifdef(sum).
+-define(sub(X,Y),{X-Y}).
+-endif.
 
 main(_) -> 
     io:fwrite("macro examples~n")
   , io:fwrite("~p~n",[?sum(1,2)])
+  , io:fwrite("~p~n",[?sub(1,5)])
 .
